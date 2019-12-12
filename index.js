4edb0c3853515350c1287fc06090ac173413d2fa
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-12-11 20:06:11 -0600

    Updated process.env for port and host and configuring it for Heroku.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express")
 
 const app = express()
-const host = "127.0.0.1"
-const port = 8080
+const host = process.env.HOST || "0.0.0.0"
+const port = process.env.PORT || 8080
 
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
